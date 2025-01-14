f926c06266fceea02302cb3edf6c988cc236a11c
HajdukSanchez <hajduksanchez.dev@gmail.com>
2025-01-14 15:11:40 -0500

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Jozek Andrzej Hajduk Sanchez on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
